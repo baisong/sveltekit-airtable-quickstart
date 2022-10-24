daddbb4b438e38761ea0035bb2a0f98f0890cdce
Oren Robinson <get.oren@gmail.com>
2022-10-24 08:33:57 -0700

    Add airtable route

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -25,6 +25,9 @@
 			<li class:active={$page.url.pathname.startsWith('/sverdle')}>
 				<a href="/sverdle">Sverdle</a>
 			</li>
+			<li class:active={$page.url.pathname.startsWith('/airtable')}>
+				<a href="/airtable">Airtable</a>
+			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
