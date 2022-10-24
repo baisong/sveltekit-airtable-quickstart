00decf45a1f3325828cfac190aa8bb6a09f3cfb0
Oren Robinson <get.oren@gmail.com>
2022-10-24 09:12:29 -0700

    Add instructions

---
+++ src/routes/airtable/+page.server.js
@@ -0,0 +1,79 @@
+import Airtable from 'airtable';
+import { env } from '$env/dynamic/private';
+
+const myBaseId = import.meta.env.VITE_AIRTABLE_BASE_ID;
+
+const myBaseConfig = {
+    tableName: 'Organizations',
+    selectConfig: {
+        pageSize: 100,
+        fields: [
+            "Org Name",
+            "Website",
+        ],
+    },
+};
+
+/**
+ * 
+ * @param {string} baseId 
+ * @returns AirtableBase
+ */
+function setupBase(baseId) {
+    Airtable.configure({
+        endpointUrl: 'https://api.airtable.com',
+        apiKey: env.AIRTABLE_KEY
+    });
+    var base = Airtable.base(baseId);
+
+    return base;
+}
+
+/**
+ * 
+ * @param {AirtableBase} base 
+ * @param {AirtableConfig} config 
+ * @returns {Array<object>} result
+ */
+async function fetchRecords(base, config) {
+    let result = [];
+    let debug = false;
+    let first = true;
+    try {
+        await base(config.tableName).select(config.selectConfig).all().then(records => {
+            records.forEach(r => {
+                let record = r.fields;
+                record.id = r.id;
+                result.push(record);
+                if (debug && first) {
+                    console.log(r);
+                    first = false;
+                }
+            });
+        }).catch(err => {
+            console.error(err);
+        });
+    } catch (e) {
+        console.error(e);
+    }
+    return result;
+}
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load({ params }) {
+    let base = await setupBase(myBaseId);
+    const records = await fetchRecords(base, myBaseConfig);
+    if (records) {
+        try {
+            return {
+                records
+            };
+        } catch (e) {
+            console.log(e.path);
+        }
+    }
+}
+
+// since there's no dynamic data here, we can prerender
+// it so that it gets served as a static asset in prod
+export const prerender = true;
--- src/routes/airtable/+page.svelte
+++ src/routes/airtable/+page.svelte
@@ -1,5 +1,10 @@
 <script>
+    import Airtable from 'airtable';
+    import { shouldListRecordsParamBePassedAsParameter } from 'airtable/lib/query_params';
+import { each } from 'svelte/internal';
 
+    /** @type {import('./$types').PageData} */
+	export let data;
 </script>
 
 <svelte:head>
@@ -8,15 +13,33 @@
 </svelte:head>
 
 <section>
-	<h1>Airtable demo</h1>
+	<h1>Airtable demo: almost there!</h1>
 
-	<h2>
-		try editing <strong>src/routes/airtable/+page.svelte</strong>
-	</h2>
+    <h3>Configuration steps:</h3>
+	<ol>
+        <li>Copy <pre>.env.example</pre> to <pre>.env</pre></li>
+		<li>Replace the example values with your actual API Key and Base ID in .env from <a href="https://airtable.com/api" target="_blank" rel="noreferrer">https://airtable.com/api</a></li>
+        <li>Try editing <strong>src/routes/airtable/+page.svelte</strong> to get your actual records and fields to display in the list below</li>
+    </ol>
 
+    <div>...That's it! You'd probably like to look at <strong>src/routes/airtable/+page.server.js</strong> to see how <pre>data</pre> is loaded.</div>
+    
+    <h3><pre>data.records</pre> length: {data.records.length}</h3>
+    <ol>
+        {#each data.records as record}
+            <!-- Note: field names are case sensitive-->
+            <li>{record["Name"]}</li>
+        {/each}
+    </ol>
 </section>
 
 <style>
+    pre {
+        display: inline
+    }
+    li {
+        margin: 0.25em;
+    }
 	section {
 		display: flex;
 		flex-direction: column;
@@ -27,21 +50,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
